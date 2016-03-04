436462cb550e65d4cb6c74a0e5bfbed214999137
Hechen Gao <hechen.gao@autodesk.com>
2016-03-04 14:31:48 -0800

    Better port mapping to make mesos local setup easier to use

---
+++ mesos/README.md
@@ -0,0 +1,24 @@
+# Mesos local setup with docker
+
+A simple but flexible mesos setup running inside docker container.
+This setup include:
+* A Zookeeper node
+  * Port mapping: 2181:2181 2888:2888 3888:3888
+* A Mesos Master node
+  * Port mapping: 5050:5050
+* A Mesos Slave node
+  * Port mapping: 5051:5051 30001-30021:1-21 30023-35050:23-5050 35052-62000:5052-32000
+  * CPU: 24
+  * Memory: 2G
+* A Marathon UI node, with port 80 open
+
+## How to Use
+* Make sure your docker daemon is running
+* Export the environment variable: MESOS_LOCALSETUP_HOST_IP to your docker daemon ip, for example:
+  * if you have boot2docker installed, you can do: ```
+  export MESOS_LOCALSETUP_HOST_IP=`boot2docker ip`
+  ```
+  * if you have docker-machine installed,  you can do: ```
+  export MESOS_LOCALSETUP_HOST_IP=`docker-machine ip mesos_local`
+  ```
+* Run ```sh start.sh``` to spin up Mesos local setup
--- mesos/start.sh
+++ mesos/start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 docker run --name mesos_local_zk -d \
   -p 2181:2181 \
@@ -5,12 +6,14 @@ docker run --name mesos_local_zk -d \
   -p 3888:3888 \
   quay.io/autodeskcloud/zookeeper
 
+sleep 5s
+
 docker run --name mesos_local_mesos_master -d \
   --net="host" \
   -p 5050:5050 \
-  -e "MESOS_HOSTNAME=${HOST_IP}" \
-  -e "MESOS_IP=${HOST_IP}" \
-  -e "MESOS_ZK=zk://${HOST_IP}:2181/mesos" \
+  -e "MESOS_HOSTNAME=${MESOS_LOCALSETUP_HOST_IP}" \
+  -e "MESOS_IP=${MESOS_LOCALSETUP_HOST_IP}" \
+  -e "MESOS_ZK=zk://${MESOS_LOCALSETUP_HOST_IP}:2181/mesos" \
   -e "MESOS_PORT=5050" \
   -e "MESOS_LOG_DIR=/var/log/mesos" \
   -e "MESOS_QUORUM=1" \
@@ -18,14 +21,20 @@ docker run --name mesos_local_mesos_master -d \
   -e "MESOS_WORK_DIR=/var/lib/mesos" \
   quay.io/autodeskcloud/mesos-master
 
+sleep 5s
+
 docker run --name mesos_local_mesos_slave -d \
   --net="host" \
   -p 5051:5051 \
-  -e "MESOS_MASTER=zk://${HOST_IP}:2181/mesos" \
-  -e "MESOS_IP=${HOST_IP}" \
+  -p 30001-30021:1-21 \
+  -p 30023-35050:23-5050 \
+  -p 35052-62000:5052-32000 \
+  -e "MESOS_MASTER=zk://${MESOS_LOCALSETUP_HOST_IP}:2181/mesos" \
+  -e "MESOS_IP=${MESOS_LOCALSETUP_HOST_IP}" \
   -e "MESOS_LOG_DIR=/var/log/mesos" \
   -e "MESOS_LOGGING_LEVEL=INFO" \
   -e "MESOS_PORT=5051" \
+  -e "MESOS_RESOURCES=cpus:24;mem:2048;ports:[1-21,23-5050,5052-32000]" \
   -e "MESOS_CONTAINERIZERS=docker" \
   -e "MESOS_DOCKER_SOCK=/var/run/docker.sock" \
   -v /sys:/sys \
@@ -33,7 +42,9 @@ docker run --name mesos_local_mesos_slave -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   quay.io/autodeskcloud/mesos-slave
 
+sleep 5s
+
 docker run --name mesos_local_marathon -d \
- -p 8080:8080 \
+ -p 80:8080 \
  quay.io/autodeskcloud/marathon \
- --master zk://${HOST_IP}:2181/mesos --zk zk://${HOST_IP}:2181/marathon
+ --master zk://${MESOS_LOCALSETUP_HOST_IP}:2181/mesos --zk zk://${MESOS_LOCALSETUP_HOST_IP}:2181/marathon
